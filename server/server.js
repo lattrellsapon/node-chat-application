c8a90cf7f0a88357363fba1c994cc44015475e7b
Lattrell Sapon <lattrellsapon@gmail.com>
2018-03-06 10:25:37 +1300

    Init Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+//joining paths
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () =>{
+
+    console.log(`Server is up on ${port}`);
+
+});
